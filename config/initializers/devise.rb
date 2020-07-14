eae0944bfbb12b77f5c46f29503b7d2aa6dde034
Vinay <vinaychandranvs@gmail.com>
2020-07-14 13:00:54 +0530

    Remove secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :google_oauth2, "121964816357-lji2qakb69duc4a0g3kaqonf531p51p1.apps.googleusercontent.com", "IQImGIbgV3yCOoiZ4GCuivTj", {}
+  config.omniauth :google_oauth2, "Google client ID", "Google Seceret", {}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
