013813242733b4749800b7bee88e3f44369424a8
Agney <agney.menon@bigbinary.com>
2020-07-15 01:47:40 +0530

    Hide instruction content on print

--- app/javascript/components/Merchant/DisplayQr.js
+++ app/javascript/components/Merchant/DisplayQr.js
@@ -25,12 +25,21 @@ function DisplayQr() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-          Your QR Code is Ready
-        </h2>
-        <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
-          Please print out this QR code and display it in a recognised area.
-        </p>
+        <div className="print:hidden">
+          <h2 className="mt-6 mb-4 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            Your QR Code is Ready
+          </h2>
+          <p className="mt-2 text-center text-sm leading-6 text-gray-600 max-w">
+            <span>
+              Please print out this QR code and display it in a recognised area.
+            </span>
+            <br />
+            <span>
+              You can print this page now by pressing Ctrl+P or selecting File
+              &lt; Print
+            </span>
+          </p>
+        </div>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-6 px-4 shadow sm:rounded-lg sm:px-10">
             <p className="mt-2 text-gray-700 leading-6 text-md text-center max-w">
@@ -45,8 +54,11 @@ function DisplayQr() {
             {qrCodeUrl && <img src={qrCodeUrl} alt="" />}
           </div>
         </div>
-        <div className="w-full mt-4 text-center ">
-          <a href="/merchant" className="text-sm leading-5 text-gray-600 max-w">
+        <div className="w-full mt-4 text-center print:hidden">
+          <a
+            href="/merchant"
+            className="text-sm leading-5 text-gray-600 max-w text-indigo-600"
+          >
             Generate another one.
           </a>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   theme: {
     extend: {
-      width: {
-        '1/7': '14.2857143%'
-      },
-      minHeight: {
-        '32': '8rem'
+      screens: {
+        print: {
+          raw: 'print'
+        },
       }
     }
   },
-  variants: {},
+  variants: {
+  },
   plugins: [
     require('@tailwindcss/ui'),
   ]
