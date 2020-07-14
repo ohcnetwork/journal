e2424236e9841c2aa386602e2196088092dfd475
Agney <agney.menon@bigbinary.com>
2020-07-14 16:50:20 +0530

    #10 Add base routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../styles";
 import "remixicon/fonts/remixicon.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default class App extends Component {
   state = {
@@ -8,6 +9,26 @@ export default class App extends Component {
   };
 
   render() {
-    return <h1 className="text-xl font-bold text-center">Hey there!</h1>;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/merchant">
+            <h1 className="text-center text-3xl leading-9 font-extrabold text-gray-900">
+              Merchant
+            </h1>
+          </Route>
+          <Route path="/admin">
+            <h1 className="text-center text-3xl leading-9 font-extrabold text-gray-900">
+              Admin
+            </h1>
+          </Route>
+          <Route path="/">
+            <h1 className="text-center text-3xl leading-9 font-extrabold text-gray-900">
+              User
+            </h1>
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 }
