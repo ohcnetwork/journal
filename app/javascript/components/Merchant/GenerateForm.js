746b1d9f7aa29efcad107ae5ad1c3304dd288dba
Agney <agney.menon@bigbinary.com>
2020-07-14 22:36:32 +0530

    Add UI for merchant form

--- app/javascript/components/Merchant/GenerateForm.js
+++ app/javascript/components/Merchant/GenerateForm.js
@@ -35,6 +35,16 @@ function GenerateForm() {
                 required
                 placeholder="Complete address of the establishment/shop/premises"
               />
+              <div className="mt-6">
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  >
+                    Generate
+                  </button>
+                </span>
+              </div>
             </form>
           </div>
         </div>
