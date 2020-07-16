f58f975bf30aeac970bc83168ea425c10c53313a
Agney <agney.menon@bigbinary.com>
2020-07-17 02:04:34 +0530

    add link to home page

--- app/javascript/components/User/Header.js
+++ app/javascript/components/User/Header.js
@@ -5,9 +5,9 @@ import Button from "../Common/Button";
 function Header() {
   return (
     <nav className="flex justify-between items-center px-4 py-4 border-b-2 border-gray-100 md:justify-start md:space-x-10">
-      <div>
+      <a href="/user">
         <h1 className="text-xl text-indigo-600 font-bold">Journal</h1>
-      </div>
+      </a>
       <div>
         <Button htmlType="button" className="border-transparent">
           Logout
