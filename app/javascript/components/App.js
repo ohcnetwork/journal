7b03ad7398cded3cf11a96907fd79341f8b76f9e
Agney <agney.menon@bigbinary.com>
2020-07-17 03:12:42 +0530

    add dayjs

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component, Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "../styles";
 import "remixicon/fonts/remixicon.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import User from "./User";
 const Merchant = lazy(() => import("./Merchant"));
 const Admin = lazy(() => import("./Admin"));
+
+dayjs.extend(relativeTime);
 
 export default class App extends Component {
   render() {
