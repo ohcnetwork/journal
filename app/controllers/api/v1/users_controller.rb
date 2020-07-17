bb119f96e30c9a2f70326f871c42e56758418705
Agney <agney.menon@bigbinary.com>
2020-07-18 03:14:26 +0530

    do not auth otp page

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < Api::V1::BaseController
+  skip_before_action :authenticate_user_using_x_auth_token!, only: [:verify_otp]
   def verify_otp
     @user = User.find(params[:id])
     if @user.otp == params[:otp]
