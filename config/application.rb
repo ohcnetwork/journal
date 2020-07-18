92c382ef540a259bf299f12e27b50c82e16429f2
Unnikrishnan KP <unni.tallman@gmail.com>
2020-07-18 12:38:22 +0530

    sets TZ for activerecord and removes views used by activeadmin

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Journal
     config.serve_static_assets = true
 
     config.time_zone = "Kolkata"
+    config.active_record.default_timezone = "Kolkata"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
