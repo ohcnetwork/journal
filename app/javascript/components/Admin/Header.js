6b3262c39789a7899b4113817acddae7b22d1d4d
Itty Jose <ittyjose@gmail.com>
2020-07-18 22:52:30 +0530

    adds the logout function for Admin console

---
+++ app/javascript/components/Admin/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import { logout } from "Apis/Admin/Auth";
+import Button from "components/Common/Button";
+
+function Header() {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    logout();
+    history.push("/admin");
+  };
+
+  return (
+    <nav className="flex relative z-10 flex-shrink-0 h-16 bg-white shadow justify-between items-center px-4 py-3 border-b-2 border-gray-100 md:justify-end md:space-x-10">
+      <div>
+        <Button
+          htmlType="button"
+          className="border-transparent"
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
