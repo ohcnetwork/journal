3063a0dc850c35668d9d0a79c3ffbbf0c753e6ec
Agney <agney.menon@bigbinary.com>
2020-07-18 15:26:53 +0530

    Admin content skelton

--- app/javascript/components/Admin/AdminHome.js
+++ app/javascript/components/Admin/AdminHome.js
@@ -21,7 +21,25 @@ function AdminHome() {
   if (loading) {
     return <p>Loading...</p>;
   }
-  return <p>AdminHome</p>;
+  return (
+    <div className="h-screen flex overflow-hidden bg-gray-100">
+      <div className="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800">
+        <a
+          href="/user"
+          className="h-16 bg-gray-900 flex justify-center items-center"
+        >
+          <h1 className="text-2xl text-indigo-600 font-bold tracking-wide">
+            Journal
+          </h1>
+        </a>
+        <nav></nav>
+      </div>
+      <div className="flex flex-col w-0 flex-1 overflow-hidden">
+        <div className="relative z-10 flex-shrink-0 flex h-16 bg-white shadow"></div>
+        <p>Admin Content</p>
+      </div>
+    </div>
+  );
 }
 
 export default AdminHome;
