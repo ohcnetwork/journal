e5fb7ab9abe6579dc382baf11ffc290b409984ac
Agney <agney.menon@bigbinary.com>
2020-07-18 15:10:58 +0530

    Add logout function

--- app/javascript/apis/Admin/Auth.js
+++ app/javascript/apis/Admin/Auth.js
@@ -1,3 +1,30 @@
 import Axios from "../axios";
 
+let authHeaderInterceptor;
+
 export const signIn = (payload) => Axios.post(`/admin/sessions`, payload);
+
+const addInterceptor = (token) => {
+  authHeaderInterceptor = Axios.interceptors.request.use((config) => {
+    return {
+      ...config,
+      headers: {
+        "X-Auth-Token": token,
+        ...config.headers,
+      },
+    };
+  });
+};
+
+export const isLoggedIn = () => {
+  const token = localStorage.getItem("admin-auth-token");
+  addInterceptor(token);
+  return token;
+};
+
+export const logout = () => {
+  localStorage.clear();
+  if (authHeaderInterceptor) {
+    Axios.interceptors.request.eject(authHeaderInterceptor);
+  }
+};
