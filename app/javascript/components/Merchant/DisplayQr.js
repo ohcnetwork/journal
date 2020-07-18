fe657e4915f8116136af3ddda0c2d75210db6b2f
Agney <agney.menon@bigbinary.com>
2020-07-18 20:17:30 +0530

    adjust qr padding

--- app/javascript/components/Merchant/DisplayQr.js
+++ app/javascript/components/Merchant/DisplayQr.js
@@ -11,7 +11,7 @@ function DisplayQr() {
   const qrCodeUrl = useGetQrUrl();
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center md:py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="print:hidden">
           <h2 className="mt-6 mb-4 text-center text-3xl leading-9 font-extrabold text-gray-900">
