d6895b8fa120f150810a216326eda92a43f0d02b
Edwin Babu <edwinbbu@gmail.com>
2020-07-25 10:37:42 +0530

    districts dropdown added

--- app/javascript/apis/Admin/index.js
+++ app/javascript/apis/Admin/index.js
@@ -23,18 +23,18 @@ const getUser = async ({ phone_number, date_of_birth }) => {
     console.log("Error while fetching user ", err);
     throw err;
   }
-}
+};
 
-export const getMerchants = async ({districtId})=>{
-  try{
+export const getMerchants = async ({ districtId }) => {
+  try {
     const url = districtId ? `/admin/merchants?district_id=${districtId}` : '/admin/merchants';
     const response = (await Axios.get(url)).data;
     return response;
-  } catch(err){
+  } catch (err) {
     console.log('Error while fetching merchants', err);
   }
 
-}
+};
 
 export const getEstablishmentRegister = async ({ visitable_id, from, to }) => {
   const dateFrom = moment(from).format("YYYY-MM-DD");
@@ -49,5 +49,14 @@ export const getEstablishmentRegister = async ({ visitable_id, from, to }) => {
   } catch (err) {
     console.log("Error while fetching Establishment ", err);
     throw new Error("Establishment not found");
+  }
+};
+
+export const getLocalBodies = async () => {
+  try {
+    const response = (await Axios.get(`/local_bodies`)).data;
+    return response;
+  } catch (err) {
+    console.log("Error while fetching local bodies ", err);
   }
 };
