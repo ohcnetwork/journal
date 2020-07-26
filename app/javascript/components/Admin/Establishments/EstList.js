531b8cbd6b550e0c672f02e0997df65058875adb
Agney <agney.menon@bigbinary.com>
2020-07-27 02:21:53 +0530

    render link to visitors page

--- app/javascript/components/Admin/Establishments/EstList.js
+++ app/javascript/components/Admin/Establishments/EstList.js
@@ -1,31 +1,43 @@
 import React from "react";
+import { useRouteMatch, Link } from "react-router-dom";
 
 import Table from "Common/Table";
 
-const columns = [
-  {
-    title: "Merchant Name",
-    dataIndex: "visitable.name",
-    className: "text-gray-900",
-  },
-  {
-    title: "Address",
-    dataIndex: "visitable.address",
-  },
-  {
-    title: "Phone",
-    dataIndex: "visitable.phone",
-  },
-];
-
 function EstList({ loading, error, data }) {
+  const match = useRouteMatch();
+
+  const renderLink = (id) => (
+    <Link to={`${match.url}/${id}`}>View Visitors</Link>
+  );
+
+  const columns = [
+    {
+      title: "Merchant Name",
+      dataIndex: "name",
+      className: "text-gray-900",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+    },
+    {
+      title: " ",
+      dataIndex: "id",
+      render: renderLink,
+    },
+  ];
+
   if (loading) {
     return <p>Loading...</p>;
   }
   if (error) {
     return <p>Could not retrieve merchant list. Please try again.</p>;
   }
-  return <Table columns={columns} data={data} />;
+  return <Table dataKey="id" columns={columns} data={data} />;
 }
 
 export default EstList;
