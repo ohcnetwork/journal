76a9a214a22c2188e6568eb48665858314cfbdfd
Agney <agney.menon@bigbinary.com>
2020-07-29 03:34:02 +0530

    fix verify otp api

--- app/javascript/apis/MerchantApi/index.js
+++ app/javascript/apis/MerchantApi/index.js
@@ -1,16 +1,20 @@
 import Axios from "../axios";
 
 export const create = async (payload) => {
-  try {
-    const response = await Axios.post("/merchants", {
-      ...payload,
-      lb_code: payload.local_body.value,
-      district: undefined,
-      local_body: undefined,
-      type: undefined,
-    });
-    return response.data;
-  } catch (err) {
-    return err.response.data;
-  }
+  const response = await Axios.post("/merchants", {
+    ...payload,
+    lb_code: payload.local_body.value,
+    district: undefined,
+    local_body: undefined,
+    type: undefined,
+  });
+  return response;
+};
+
+export const verifyOtp = async (tempId, otp) => {
+  return await Axios.post(`/merchants/${tempId}/verify_otp`, null, {
+    params: {
+      otp,
+    },
+  });
 };
