64cb600ced23fe17770cc7c7f1f1b347a8998659
Unnikrishnan KP <unni.tallman@gmail.com>
2020-07-28 13:37:30 +0530

    6: otp token field added to merchants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_051713) do
+ActiveRecord::Schema.define(version: 2020_07_28_080517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_051713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "lb_code"
+    t.text "otp_token"
   end
 
   create_table "qr_codes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
