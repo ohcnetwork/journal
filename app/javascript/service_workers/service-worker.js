a44b759610cf8f7a52868f622595c25b9624a672
Unnikrishnan KP <unni.tallman@gmail.com>
2020-08-17 15:21:42 +0530

    service worker, manifest.json and icons added

--- app/javascript/service_workers/service-worker.js
+++ app/javascript/service_workers/service-worker.js
@@ -1,10 +1,10 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
+self.addEventListener("install", function (event) {
+  console.log("Service Worker installing.");
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activated.');
+self.addEventListener("activate", function (event) {
+  console.log("Service Worker activated.");
 });
-self.addEventListener('fetch', function(event) {
-  console.log('Service Worker fetching.');
+self.addEventListener("fetch", function (event) {
+  console.log("Service Worker fetching.");
 });
